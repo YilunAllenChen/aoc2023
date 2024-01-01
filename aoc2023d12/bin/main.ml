63d80f6e586dcb153674e507f089156e016db00e
Allen Chen <alchen@drwholdings.com>
2024-01-01 11:51:58 -0600

    d12 pt1

--- aoc2023d12/bin/main.ml
+++ aoc2023d12/bin/main.ml
@@ -2,7 +2,9 @@ open Base
 open Stdio
 
 type loc = Oper | Dmgd | Unkn [@@deriving sexp_of]
-type line = { locs : loc array; stats : int array } [@@deriving sexp_of]
+
+type line = { locs : loc array; raw : string; stats : int array }
+[@@deriving sexp_of]
 
 let free_locs_in_line line =
   let dmged_recorded = Array.fold line.stats ~init:0 ~f:( + ) in
@@ -23,16 +25,21 @@ let get_free_pos line =
       if phys_equal x Unkn then Some i else None)
 
 let is_valid_line line =
-    if Array.length line.stats = 0 then
-        true
-    else
-        false  (* TODO *)
+  let pattern = Re2.of_string "(#)+" in
+  let all_matches = Re2.find_all_exn pattern line.raw in
+  all_matches |> List.map ~f:String.length |> Array.of_list
+  |> Array.equal (fun a b -> a = b) line.stats
 
 let char_to_loc = function
   | '.' -> Oper
   | '#' -> Dmgd
   | '?' -> Unkn
   | _ -> failwith "Invalid char"
+
+let loc_to_char = function
+  | Oper -> "."
+  | Dmgd -> "#"
+  | _ -> failwith "can't do that"
 
 let parse_line line =
   match String.split line ~on:' ' with
@@ -44,14 +51,13 @@ let parse_line line =
       let locs =
         locs |> String.to_list |> List.map ~f:char_to_loc |> Array.of_list
       in
-      { locs; stats }
+      { locs; stats; raw = line }
   | _ -> failwith "Invalid line"
 
 let rec permutations a_c b_c =
   match (a_c, b_c) with
   | 0, 0 -> [ [] ]
   | _, _ ->
-      printf "a_c: %d ; b_c: %d\n" a_c b_c;
       let with_dmgd =
         if a_c > 0 then
           List.map ~f:(fun perm -> Dmgd :: perm) (permutations (a_c - 1) b_c)
@@ -64,7 +70,7 @@ let rec permutations a_c b_c =
       in
       with_dmgd @ with_op
 
-let get_strategies line =
+let get_valid_strategies line =
   let free_pos = get_free_pos line |> List.of_array in
   let dmgd_to_place, ops_to_place = free_locs_in_line line in
   let perms = permutations dmgd_to_place ops_to_place in
@@ -72,23 +78,19 @@ let get_strategies line =
       let assignments = List.zip_exn free_pos perm in
       let new_locs = Array.copy line.locs in
       List.iter assignments ~f:(fun (pos, loc) -> new_locs.(pos) <- loc);
-      { locs = new_locs; stats = line.stats })
+      let raw =
+        Array.map new_locs ~f:loc_to_char |> List.of_array |> String.concat
+      in
+      { locs = new_locs; stats = line.stats; raw })
   |> List.filter ~f:is_valid_line
-  |> [%sexp_of: line list] |> Sexp.to_string_hum |> print_endline
+  (* |> [%sexp_of: line list] |> Sexp.to_string_hum |> print_endline *)
 
 let () =
   let data = In_channel.read_lines "data" |> List.map ~f:parse_line in
 
-  "???..###. 1,1,3" |> parse_line |> get_strategies;
-  ignore is_valid_line;
+  "????.######..#####. 1,6,5" |> parse_line |> get_valid_strategies |> List.length |> printf "%d valid strategies\n";
 
-  data |> List.map ~f:free_locs_in_line |> ignore;
-
-  (* |> List.iter ~f:(fun (a, b) -> printf "dmg: %d ; ops: %d\n" a b); *)
-  data |> List.map ~f:get_free_pos
-  |> List.iter ~f:(fun x ->
-         x |> Array.map ~f:Int.to_string |> Array.to_list
-         |> String.concat ~sep:"," |> ignore)
-(* |> print_endline) *)
-
-(* data |> [%sexp_of: line list] |> Sexp.to_string_hum |> print_endline *)
+  data
+  |> List.map ~f:(fun line -> line |> get_valid_strategies |> List.length)
+  |> List.fold ~init:0 ~f:( + )
+  |> printf "Valid Arrangement Sum: %d\n"
